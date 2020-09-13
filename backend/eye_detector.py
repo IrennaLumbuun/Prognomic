03918adb1ab0198512a9478e9a682183cc15b8d4
Irenna Lumbuun <irennanicole1@gmail.com>
2020-09-12 17:12:43 -0700

    Going back to vision api because haarclassifier messed things up :(

--- backend/eye_detector.py
+++ backend/eye_detector.py
@@ -4,45 +4,48 @@ from keras.models import load_model
 from PIL import Image
 import cv2
 import numpy as np
-import base64
 
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.protobuf.json_format import MessageToDict
 
 client = vision.ImageAnnotatorClient()
-eye_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + 'haarcascade_eye.xml')
-
-
-def detect_eyes(image):
-    img = Image.open(image)
-    img = np.array(img)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    eyes = eye_cascade.detectMultiScale(img)
-    crop_boundary = list()
-    for (ex, ey, ew, eh) in eyes:
-        bound = [ex, ey, ex + ew, ey + eh]
-        crop_boundary.append(bound)
-    print(crop_boundary)
-    return crop_boundary
 
 
 def handle_image(image):
     img_copy = image  # safe a copy of the image for future use (e.g cropping)
-    crop_boundary = detect_eyes(img_copy)
+    b64_image = image.read()  # decoded image
+
+    # get_eyes position
+    eyes_pos = get_eyes(b64_image)
+
+    if eyes_pos == []:
+        return "Can't find face"
+
+    # crop boundary consits of [(upper left left eye), (bottom right right eye), (upper left right eye), {bottom right tight eye}]
+    crop_boundary = get_crop_boundary(eyes_pos)
+
     images = list()  # replace with a list of cropped & resized image
 
     # crop image and reshape images
     im = Image.open(img_copy)
     for bounds in crop_boundary:
-        eye = im.crop((bounds[0], bounds[1], bounds[2], bounds[3]))
+        # crop
+        left_eye = im.crop((bounds[0][0], bounds[0][1],
+                            bounds[1][0], bounds[1][1]))
         # resize & convert to grayscale
-        eye = np.array(eye)
-        eye = cv2.cvtColor(eye, cv2.COLOR_BGR2GRAY)
-        eye = cv2.resize(eye, (151, 332))
+        left_eye = np.array(left_eye)
+        left_eye = cv2.cvtColor(left_eye, cv2.COLOR_BGR2GRAY)
+        left_eye = cv2.resize(left_eye, (151, 332))
 
-        images.append(eye)
+        images.append(left_eye)
+        if len(bounds) == 4:
+            right_eye = im.crop((
+                bounds[2][0], bounds[2][1], bounds[3][0], bounds[3][1]))
+            # resize & convert to grayscale
+            right_eye = np.array(right_eye)
+            right_eye = cv2.cvtColor(right_eye, cv2.COLOR_BGR2GRAY)
+            right_eye = cv2.resize(right_eye, (151, 332))
 
     # pass to model
     model = load_model('backend/trained_dataset.h5')
@@ -50,11 +53,80 @@ def handle_image(image):
     result = dict()
     for img in images:
         img = img.reshape((1, 50132)).tolist()
+        print(len(img))
         output = model.predict(img)
-        print(output)
         output = output.flatten().tolist()
         for i in range(len(interpret)):
             result[interpret[i]] = output[i]
         print(result)
-
     return result
+
+
+def get_crop_boundary(eyes_pos):
+    crop_boundary = list()
+    for pos in eyes_pos:
+        bound = list()
+        # left eye
+        if pos.get('left_eye', None) != None:
+            # left eye exists
+            upper_left_left_eye = [pos.get('left_eye_left_corner')[
+                'x'], pos.get('left_eye_top_boundary', 0)['y']]
+            bottom_right_left_eye = [pos.get('left_eye_right_corner')[
+                'x'], pos.get('left_eye_bottom_boundary', 0)['y']]
+        # right eye
+        if pos.get('right_eye', None) != None:
+            # right eye exists
+            upper_left_right_eye = [pos.get('right_eye_left_corner')[
+                'x'], pos.get('right_eye_top_boundary', 0)['y']]
+            bottom_right_right_eye = [pos.get('right_eye_right_corner')[
+                'x'], pos.get('right_eye_bottom_boundary', 0)['y']]
+
+            bound.append(upper_left_left_eye)
+            bound.append(bottom_right_left_eye)
+            bound.append(upper_left_right_eye)
+            bound.append(bottom_right_right_eye)
+            if bound != [] and len(bound) % 2 == 0:
+                crop_boundary.append(bound)
+    return crop_boundary
+
+
+def get_eyes(b64_image):
+    request = {
+        'image': {
+            'content': b64_image
+        },
+        'features': [{
+            'type': vision.enums.Feature.Type.FACE_DETECTION
+        }]
+    }
+    # parse response
+    response = client.annotate_image(request)
+    response = MessageToDict(response)
+
+    try:
+        faces = response['faceAnnotations']
+    except:
+        print("No face")
+        return list()
+    eyes_pos = list()  # save coordinate of left eye and right eye
+
+    info_needed = ['LEFT_EYE', 'RIGHT_EYE', 'RIGHT_EYE_LEFT_CORNER', 'RIGHT_EYE_BOTTOM_BOUNDARY',
+                   'RIGHT_EYE_RIGHT_CORNER', 'RIGHT_EYE_TOP_BOUNDARY', 'LEFT_EYE_LEFT_CORNER', 'LEFT_EYE_BOTTOM_BOUNDARY',
+                   'LEFT_EYE_RIGHT_CORNER', 'LEFT_EYE_TOP_BOUNDARY']
+
+    for face in faces:
+        landmark_pos = dict()
+        try:
+            landmarks = face['landmarks']
+            for landmark in landmarks:
+                landmark_type = landmark.get('type', '')
+                if landmark_type in info_needed:
+                    landmark_pos[landmark_type.lower()] = landmark['position']
+            eyes_pos.append(landmark_pos)
+        except Exception as e:
+            print('--------errror---------')
+            print(response)
+            print(e)
+            return eyes_pos
+
+    return eyes_pos
